6e9a03e3beee1b5285deb9d9a1a5ac7da037e2d3
UserJue <UserJue58@gmail.com>
2017-03-14 22:48:16 +0100

    add missing files

---
+++ 15Puzzle/15Puzzle/_15Puzzle/Converters/ImageConverter..cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace _15Puzzle.Converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var name = value as string;
+            if (name == null) return null;
+            var source = "_15Puzzle.Pictures." + name + ".png";
+            return ImageSource.FromResource(source);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
